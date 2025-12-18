e906b782fe5fdc98b5790677bd142860eb094ff9
elsyaameliam-netizen <elsyaameliam@gmail.com>
2025-12-18 21:23:40 +0700

    update

--- Modul 1-ANATOMI & MEMORI (Variables & Data Types)/SesiSatu.java
+++ Modul 1-ANATOMI & MEMORI (Variables & Data Types)/SesiSatu.java
@@ -4,7 +4,12 @@ public class SesiSatu {
         System.out.println("=== SYSTEM BOOT ===");
         System.out.print("Loading...");
         System.out.println("Done!");
-        System.out.println("Halo, Mahasiswa [NAMA]!"); 
+        System.out.println("Halo, Mahasiswa [ELSYA]!");
+        System.out.println("Hai,Aku suka ice cream");
+        System.out.println("Harus bisa harusnya sih");
+        System.out.println("Encok deh aku");
+        System.out.println("Ya haloooo");
+    
         // Coba hapus tanda ; di baris atas, lihat apa yang terjadi di VS Code?
     }
 }
